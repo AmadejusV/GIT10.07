6ccb2a09c83070e9f2eca43b61a4ff98ecc2c4d2
Amis <Moksleivis@KITM.local>
2020-10-07 10:29:30 +0300

    dar pakeitimu

--- PirmaPamoka/Program.cs
+++ PirmaPamoka/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             //Prirasytas tekstas del paketimu i github siust
+            //dsdfsf
 
             static void add(){
                 Console.WriteLine("Enter the first number: ");
